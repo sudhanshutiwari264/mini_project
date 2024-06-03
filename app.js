13e21a9f7d12520ea5e79573dce2e06727d9b58a
Sudhanshu tiwari <sudhanshutiwari264@gmail.com>
2024-06-04 00:05:16 +0530

    initial

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require(`express`);
+const app = express();
+const userModel = require('./models/user')
+const postModel = require('./models/post')
+
+const path = require(`path`);
+const bcrypt = require(`bcrypt`);
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+
+app.set('view engine','ejs')
+app.use(express.urlencoded({extended:true}));
+app.use(express.json())
+app.use(cookieParser())
+app.set(express.static(path.join(__dirname,'public')));
+
+app.get('/',(req, res)=>{
+  res.render('index')
+});
+
+app.get('/login',( req, res)=>{
+  res.render('login')
+});
+
+app.post('/register',async (req, res)=>{
+  let { username, name, age, email, password} = req.body;
+  let user = await userModel.findOne({email});
+  if(user) return res.status(500).send('User already registered')
+
+  bcrypt.genSalt(10,(err, salt)=>{
+    bcrypt.hash(password,salt,async (err, hash)=>{
+     let user = await userModel.create({
+                        username,
+                        name,
+                        age,
+                        email,
+                        password: hash
+                      });
+      let token = jwt.sign({ email: user.name,userid: user._id }, 'shhhhh');
+      res.cookie('token',token);
+      res.send('registered')
+    })
+  });
+});
+
+app.get('/login',( req, res)=>{
+  res.render('login')
+});
+
+app.post('/login',async (req, res)=>{
+  let { email, password } = req.body;
+  let user = await userModel.findOne({email});
+  if(!user) res.status(500).send('something went wrong email');
+
+  bcrypt.compare(password,user.password,(err,result)=>{
+    if(result) res.status(200).send('You can login')
+    res.redirect('/login')
+
+  })
+});
+
+app.get('/logout',(req, res)=>{
+  res.cookie('token','')
+  res.send('logout')
+})
+
+app.listen(3000);
