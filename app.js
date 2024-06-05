684ecbc77aae2887d7ff5ae1c6c18cb855d29423
Sudhanshu tiwari <sudhanshutiwari264@gmail.com>
2024-06-05 23:32:33 +0530

    chore: Add profile route, authentication middleware, and user model update

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require(`path`);
 const bcrypt = require(`bcrypt`);
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
+const user = require('./models/user');
 
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended:true}));
@@ -43,13 +44,44 @@ app.post('/register',async (req, res)=>{
   });
 });
 
-app.get('/login',( req, res)=>{
-  res.render('login')
+app.get('/profile', isLoggedIn,async ( req, res)=>{
+  let user =  await userModel.findOne({email: req.user.email}).populate('posts')
+  //console.log(user.posts)
+  res.render('profile',{user})
 });
 
-app.get('/profile', isLoggedIn,( req, res)=>{
-  console.log(req.user)
-  res.redirect('/login')
+app.get('/like/:id', isLoggedIn, async ( req, res)=>{
+  let post =  await postModel.findOne({_id: req.params.id}).populate('user')
+  if(post.likes.indexOf(req.user.userid) === -1){
+    post.likes.push(req.user.userid)
+  }else{
+    post.likes.splice(post.likes.indexOf(req.user.userid),1)
+  }
+  await post.save()
+  res.redirect('/profile')
+});
+
+app.get('/edit/:id', isLoggedIn, async ( req, res)=>{
+  let post =  await postModel.findOne({_id: req.params.id}).populate('user')
+
+  res.render('edit',{post})
+});
+
+app.post('/update/:postid', isLoggedIn, async ( req, res)=>{
+  let post =  await postModel.findOneAndUpdate({_id: req.params.postid},{content:req.body.content})
+  res.redirect('/profile')
+});
+
+app.post('/post', isLoggedIn,async ( req, res)=>{
+  let user =  await userModel.findOne({email: req.user.email})
+  let {content} = req.body 
+  let post = await postModel.create({
+    user: user._id,
+    content,
+  })
+  user.posts.push(post._id)
+  await user.save();
+  res.redirect('/profile')
 });
 
 app.post('/login',async (req, res)=>{
@@ -59,9 +91,9 @@ app.post('/login',async (req, res)=>{
 
   bcrypt.compare(password,user.password,(err,result)=>{
     if(result){ 
-    let token = jwt.sign({ email: user.name,userid: user._id }, 'shhhhh');
+    let token = jwt.sign({ email: user.email,userid: user._id }, 'shhhhh');
     res.cookie('token',token);
-    res.status(200).send('loggend in')   
+    res.status(200).redirect('/profile')   
     }else{
        res.status(500).send('something went wrong pass')}
   })
@@ -76,7 +108,7 @@ app.get('/logout',(req, res)=>{
 function isLoggedIn(req, res, next) {
   const token = req.cookies.token;
   if (!token) {
-    return res.status(401).send('You must log in');
+    return res.status(401).redirect('/login');
   }
 
   jwt.verify(token, 'shhhhh', (err, decoded) => {
